628a2fd474ec19733995488e71eb751b3097dc30
anishkumarshrees <anishshrees12@gmail.com>
2025-05-10 21:12:23 +0545

    react hook update

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,13 +1,35 @@
+import { useState } from "react"
 
 
 
 function Home(){
+    // let something = useState(100) //[100,f] array called vako xa first maa 100 and second maa function aako xa
+    // let myValue = something[0] //useState ko first ko value call gareko
+    // let myFunction = something[1] //useState ko second index call gareko
+    let[number,setNumber]= useState(0) //mathi ko code eti maa lekhna milyo
+    function increaseMyValue(){
+        // myValue = myValue + 1 yo garna milena react maa
+        // myFunction(myValue + 1)
+        setNumber(number +1)
+        console.log(number)
+    }
+    function decreaseMyValue(){
+        // myValue = myValue + 1 yo garna milena react maa
+        // myFunction(myValue + 1)
+        setNumber(number -1)
+        console.log(number)
+    }
+
+
+
     return(
 
 <>
-<h1>this is main home page</h1>
+
 <div>
-    <nav></nav>
+    <h1>{number}</h1>
+    <button onClick={increaseMyValue} style={{width: "100px", height:"50px"}}>+</button>
+    <button onClick={decreaseMyValue}>-</button>
 </div>
 </>
     )
